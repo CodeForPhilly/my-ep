5deb435c5e6450c02e7ab626889c11642a3e967e
Brad Steinberg <brad@Brads-MacBook.local>
2015-10-18 12:41:43 -0400

    app.js now has scrolling feature for top nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 $(document).ready(function() {
-	// body...
-})
+	"use strict";
+	$(".home-nav").on("click", function(){
+	    scrollToIt(".home-jumbo");
+	});
+
+	$(".team-nav").on("click", function(){
+	    scrollToIt("#parent-jumbo");
+	});
+
+	$(".meeting-nav").on("click", function(){
+	    scrollToIt(".meetings-jumbo");
+	});
+
+	$(".questions-nav").on("click", function(){
+	    scrollToIt(".contact-jumbo");
+	});
+
+
+	function scrollToIt(className) {
+		$('html, body').animate({
+	    	scrollTop: ($(className).offset().top)
+	    }, 500);
+	}
+});
